d27cc3aa2baaf9d9d525bbfad1a00ccbadb7885e
Phillip Peter Kersten <pkersten@test3.irc.sara.nl.irc.sara.nl>
2019-10-14 15:07:26 +0200

    Cannot index with multidimensional key

--- analyse_data_transformation.py
+++ analyse_data_transformation.py
@@ -142,7 +142,8 @@ def make_word_measures_bylength(df_alldata):
     df_only_word_activity = df_only_word_activity.loc[:,(df_only_word_activity.any(axis=0)!=0)]
     df_only_word_activity.replace(0.0, np.nan)
     df_only_word_activity = pd.concat([df_only_word_activity,df_alldata['word length']], axis=1, join_axes=[df_only_word_activity.index])
-    word_measures_dict['activity'] = df_only_word_activity.groupby('word length').mean()
+    word_measures_dict['activity'] = word_measures_dict['activity'].apply(pd.to_numeric)
+    word_measures_dict['activity'] = df_only_word_activity.groupby('word length').apply(pd.to_numeric).mean()
     #df_only_word_activity_grpby_length = df_only_word_activity.groupby('word length').agg(np.mean)
 
     for i in xrange(len(complete_threshold_matrix[0,:])):
@@ -150,31 +151,31 @@ def make_word_measures_bylength(df_alldata):
     df_only_word_threshold = pd.DataFrame(complete_threshold_matrix, dtype=float)
     df_only_word_threshold = pd.concat([df_only_word_threshold, df_alldata['word length']], axis=1, join_axes=[df_only_word_threshold.index])
     df_only_word_threshold = df_only_word_threshold.loc[:,(df_only_word_threshold.any(axis=0)!=0)]
-    word_measures_dict['threshold'] = df_only_word_threshold.groupby('word length').mean()
+    word_measures_dict['threshold'] = df_only_word_threshold.groupby('word length').apply(pd.to_numeric).mean()
     #df_only_word_threshold_grpby_length = df_only_word_threshold.groupby('word length').mean()
 
     df_only_word_excitation = pd.DataFrame(complete_excitation_matrix, dtype=float)
     df_only_word_excitation = pd.concat([df_only_word_excitation,df_alldata['word length']], axis=1, join_axes=[df_only_word_excitation.index])
     df_only_word_excitation = df_only_word_excitation.loc[:,(df_only_word_excitation.any(axis=0)!=0)]
-    word_measures_dict['excitation'] = df_only_word_excitation.groupby('word length').mean()
+    word_measures_dict['excitation'] = df_only_word_excitation.groupby('word length').apply(pd.to_numeric).mean()
     #df_only_word_excitation_grpby_length = df_only_word_excitation.groupby('word length').mean()
 
     df_only_word_inhibition = pd.DataFrame(complete_inhibition_matrix, dtype=float)
     df_only_word_inhibition = pd.concat([df_only_word_inhibition,df_alldata['word length']], axis=1, join_axes=[df_only_word_inhibition.index])
     df_only_word_inhibition = df_only_word_inhibition.loc[:,(df_only_word_inhibition.any(axis=0)!=0)]
-    #word_measures_dict['inhibition'] = df_only_word_inhibition.groupby('word length').mean()
+    #word_measures_dict['inhibition'] = df_only_word_inhibition.groupby('word length').mean() # TODO!: Ask why this is commented
     #df_only_word_inhibition_grpby_length = df_only_word_inhibition.groupby('word length').mean()
 
     df_only_word_realactivity = pd.DataFrame(complete_realactivity_matrix, dtype=float)
     df_only_word_realactivity = pd.concat([df_only_word_realactivity,df_alldata['word length']], axis=1, join_axes=[df_only_word_realactivity.index])
     df_only_word_realactivity = df_only_word_realactivity.loc[:,(df_only_word_realactivity.any(axis=0)!=0)]
-    word_measures_dict['realactivation'] = df_only_word_realactivity.groupby('word length').mean()
+    word_measures_dict['realactivation'] = df_only_word_realactivity.groupby('word length').apply(pd.to_numeric).mean()
     #df_only_word_realactivity_grpby_length = df_only_word_realactivity.groupby('word length').mean()
 
     df_only_word_decay = pd.DataFrame(complete_decay_matrix, dtype=float)
     df_only_word_decay = pd.concat([df_only_word_decay,df_alldata['word length']], axis=1, join_axes=[df_only_word_decay.index])
     df_only_word_decay = df_only_word_decay.loc[:,(df_only_word_decay.any(axis=0)!=0)]
-    word_measures_dict['decay'] = df_only_word_decay.groupby('word length').mean()
+    word_measures_dict['decay'] = df_only_word_decay.groupby('word length').apply(pd.to_numeric).mean()
     #df_only_word_decay_grpby_length = df_only_word_decay.groupby('word length').mean()
 
     return word_measures_dict

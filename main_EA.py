f2119e25248f7e52909ddb4ce14aec84ba9607da
PhPeKe <p.kersten@live.de>
2020-03-11 19:53:54 +0100

    adjusted EA

--- main_EA.py
+++ main_EA.py
@@ -28,39 +28,39 @@ import pandas as pd
 from get_parameters import get_params
 from joblib import Parallel, delayed
 from copy import copy
+import itertools
 
 import random
 from deap import creator, base, tools, algorithms
-from scoop import futures
 
 # Init distance variables for the reading function used in tuning
 OLD_DISTANCE = np.inf
 N_RUNS = 0
 
 
-def main():
-	# Get parameters for tuning
-	parameters, bounds, names = get_params(pm)
+#def main():
+# Get parameters for tuning
+parameters, bounds, names = get_params(pm)
 
-	# Reading function for tuning (called by scipy's L-BFGS-B optimizing function)
-	def reading_function(parameters_rf):
-		global OLD_DISTANCE
-		global N_RUNS
-		filename = "PSC_ALL"
-		filepath_psc = "PSC/" + filename + ".txt"
-	### For testing (loading past results instead of running simulation)
-	#	with open("Results/all_data.pkl","r") as f:
-	#		all_data = pickle.load(f)
-	#	with open("Results/unrecognized.pkl","r") as f:
-	#		unrecognized_words = pickle.load(f)
-	###
-	        # Run the simulation
-	        (lexicon, all_data, unrecognized_words) = reading_simulation(filepath_psc, parameters_rf)
-	        # Evaluate run and retrieve error-metric
-	        distance = get_scores(filename, all_data, unrecognized_words)
+# Reading function for tuning (called by scipy's L-BFGS-B optimizing function)
+def reading_function(parameters_rf):
+	global OLD_DISTANCE
+	global N_RUNS
+	filename = "PSC_ALL"
+	filepath_psc = "PSC/" + filename + ".txt"
+### For testing (loading past results instead of running simulation)
+#	with open("Results/all_data.pkl","r") as f:
+#		all_data = pickle.load(f)
+#	with open("Results/unrecognized.pkl","r") as f:
+#		unrecognized_words = pickle.load(f)
+###
+		# Run the simulation
+	(lexicon, all_data, unrecognized_words) = reading_simulation(filepath_psc, parameters_rf)
+	# Evaluate run and retrieve error-metric
+	distance = get_scores(filename, all_data, unrecognized_words)
 
-	        # Save parameters when distance is better than previous
-	        write_out = pd.DataFrame(np.array([names, parameters_rf]).T)
+	# Save parameters when distance is better than previous
+	write_out = pd.DataFrame(np.array([names, parameters_rf]).T)
 #	        if distance < OLD_DISTANCE:
 #	                write_out.to_csv(str(distance)+"_"+pm.tuning_measure+"parameters.txt", index=False, header=["name", "value"])
 #	                OLD_DISTANCE = distance
@@ -77,112 +77,109 @@ def main():
 #	                f.write("run "+str(N_RUNS)+": "+str(int(distance))+"\n")
 #			f.write(p)
 #			f.write("\n")
-	        N_RUNS += 1
-	        return (distance,)
+	N_RUNS += 1
+	return (distance,)
 
-	if pm.language == "german":
-		filename = "PSC_ALL"
-		filepath_psc = "PSC/" + filename + ".txt"
-	# The reading model reads dutch but there is no data to compare it to yet
-	if pm.language == "dutch":
-		raise NotImplementedError
-		filename = "PSC/words_dutch.pkl"
+if pm.language == "german":
+	filename = "PSC_ALL"
+	filepath_psc = "PSC/" + filename + ".txt"
+# The reading model reads dutch but there is no data to compare it to yet
+if pm.language == "dutch":
+	raise NotImplementedError
+	filename = "PSC/words_dutch.pkl"
 
-	output_file_all_data, output_file_unrecognized_words = ("Results/all_data"+pm.language+".pkl","Results/unrecognized"+pm.language+".pkl")
-	start_time = time.time()
+output_file_all_data, output_file_unrecognized_words = ("Results/all_data"+pm.language+".pkl","Results/unrecognized"+pm.language+".pkl")
+start_time = time.time()
 
-	if pm.run_exp:
-		# Run the reading model
-		(lexicon, all_data, unrecognized_words) = reading_simulation(filepath_psc, parameters=[])
-		# Save results: all_data...
-		all_data_file = open(output_file_all_data,"w")
-		pickle.dump(all_data, all_data_file)
-		all_data_file.close()
-		# ...and unrecognized words
-		unrecognized_file = open(output_file_unrecognized_words, "w")
-		pickle.dump(unrecognized_words, unrecognized_file)
-		unrecognized_file.close()
+if pm.run_exp:
+	# Run the reading model
+	(lexicon, all_data, unrecognized_words) = reading_simulation(filepath_psc, parameters=[])
+	# Save results: all_data...
+	all_data_file = open(output_file_all_data,"w")
+	pickle.dump(all_data, all_data_file)
+	all_data_file.close()
+	# ...and unrecognized words
+	unrecognized_file = open(output_file_unrecognized_words, "w")
+	pickle.dump(unrecognized_words, unrecognized_file)
+	unrecognized_file.close()
 
-	if pm.analyze_results:
-		get_results(filepath_psc,output_file_all_data,output_file_unrecognized_words)
+if pm.analyze_results:
+	get_results(filepath_psc,output_file_all_data,output_file_unrecognized_words)
 
-	if pm.optimize:
-		# EA parameters
-		pop_size = 32
-		multi_processing = True
-		gens = 10
-		tournament_size = 5
-		cx_prob = 0.2
-		# Mutation = +-10%
-		mut_size = 0.1
-		mut_prob = 0.1
+if pm.optimize:
+	# EA parameters
+	pop_size = 32
+	multi_processing = True
+	gens = 10
+	tournament_size = 5
+	cx_prob = 0.2
+	# Mutation = +-10%
+	mut_size = 0.1
+	mut_prob = 0.1
 
-		# Create fitness
-		creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+	# Create fitness
+	creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
 
-		# Create Individual
-		creator.create("Individual", list, fitness=creator.FitnessMin)
+	# Create Individual
+	creator.create("Individual", list, fitness=creator.FitnessMin)
 
-		# Register attributes with bounds
-		# TODO: evenly spread parameters (with for loop or similar), !!itertools combination!!
-		# use 3 points
-		def get_param(param):
-			init = random.randint(0,2)
-			if init == 0:
-				return param - (param/2.0)
-			if init == 1:
-				return param
-			if init == 2:
-				return param * 2.0
+	# Register attributes with bounds
+	# use 3 points
+	def get_param(param):
+		init = random.randint(0, 2)
+		if init == 0:
+			return param - (param/2.0)
+		if init == 1:
+			return param
+		if init == 2:
+			return param * 2.0
 
-		def mutate(gen, p):
-			print("-----------------------\nGene before\n"+str(gen))
-			for i, allele in enumerate(gen):
-				# Mutation takes place
-				if random.uniform(0,1) > p:
-					print("Mutation of gene "+str(gen[i]))
-					# Either + or - 10% of the original value
-					gen[i] = allele + (allele * mut_size) if random.randint(0,1) else allele - (allele * mut_size)
-					print("to: "+str(gen[i]))
-			print("Gene after\n"+str(gen)+"\n-----------------------")
-			return (gen,)
+	def high(x):
+		return x*2
+
+	def low(x):
+		return x/2
 
 
-		toolbox = base.Toolbox()
-		toolbox.register("attention_skew", get_param, 4.0)
-		toolbox.register("salience_position", get_param, 1.29)
-		toolbox.register("sacc_optimal_distance", get_param, 7.0)
-		toolbox.register("mu", get_param, 4.9)
-		toolbox.register("sigma", get_param, 2.2)
-		toolbox.register("distribution_param", get_param, 1.1)
-
-		# Register Individual
-		toolbox.register("Individual", tools.initCycle, creator.Individual,
-				(toolbox.attention_skew,
-				 toolbox.salience_position,
-				 toolbox.sacc_optimal_distance,
-				 toolbox.mu,
-				 toolbox.sigma,
-				 toolbox.distribution_param),
-				n=1)
-
-		# Register population
-		toolbox.register("population", tools.initRepeat, list, toolbox.Individual)
-
-		# Enable parallel processing
-#		if multi_processing:
-#			toolbox.register("map", futures.map)
-
-		# Define operators
-		toolbox.register("mate", tools.cxOnePoint)
-		toolbox.register("mutate", mutate, p=mut_prob)
-		toolbox.register("select", tools.selTournament, tournsize=tournament_size)
-		toolbox.register("evaluate", reading_function)
-
-		# Initialize first population
-		population = toolbox.population(n=pop_size)
+	def mutate(gen, p):
+		print("-----------------------\nGene before\n"+str(gen))
+		for i, allele in enumerate(gen):
+			# Mutation takes place
+			if random.uniform(0,1) > p:
+				print("Mutation of gene "+str(gen[i]))
+				# Either + or - 10% of the original value
+				gen[i] = allele + (allele * mut_size) if random.randint(0, 1) else allele - (allele * mut_size)
+				print("to: "+str(gen[i]))
+		print("Gene after\n"+str(gen)+"\n-----------------------")
+		return gen,
 
 
+	def initIndividual(icls, content):
+		return icls(content)
+
+
+	def initPopulation(pcls, ind_init):
+		n_params = 5
+		combinaties = [list(x) for x in list(itertools.product([high, low], repeat=n_params))]
+		start_params = [4.0, 1.29, 7.0, 4.9, 2.2]
+		contents = [[y(z) for y, z in zip(x, start_params)] for x in combinaties]
+		return pcls(ind_init(c) for c in contents)
+
+
+	toolbox = base.Toolbox()
+
+	toolbox.register("individual_guess", initIndividual, creator.Individual)
+	toolbox.register("population_guess", initPopulation, list, toolbox.individual_guess)
+
+	population = toolbox.population_guess()
+
+	# Define operators
+	toolbox.register("mate", tools.cxOnePoint)
+	toolbox.register("mutate", mutate, p=mut_prob)
+	toolbox.register("select", tools.selTournament, tournsize=tournament_size)
+	toolbox.register("evaluate", reading_function)
+
+"""
 		# Evaluate first population
 		print("Start evaluation gen 0")
 #		fits = Parallel(n_jobs=16, prefer="threads")(delayed(toolbox.evaluate)(ind) for ind in population)
@@ -224,3 +221,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
+"""
